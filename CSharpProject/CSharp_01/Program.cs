59184e629573d364c6ae3ca8799e73e43bae6212
DESKTOP-PN3T8HH\정창식 <popo6511@gmail.com>
2026-07-14 20:56:34 +0900

    1주차 : C# 기초 문법 학습

--- CSharpProject/CSharp_01/Program.cs
+++ CSharpProject/CSharp_01/Program.cs
@@ -5,21 +5,38 @@ class Program
     static void Main(string[] args)
     {
         // 변수 선언 - 타입 변수명 = 초깃값;
-        int level = 1;  // 정수 (int, integer)
+        string playerName = "Zack";
+        int level;  // 정수 (int, integer)
         float hp = 100.0f;  // 실수 (float)
+        bool isDead = false;
+
+        level = 1;
         
         Console.WriteLine("게임 개발자의 세계로 오신 것을 환영합니다!");
+        Console.WriteLine();
         
-        // Console.Write(level);
-        // Console.Write(hp);
+        // 문장 넘기기(\n) 없이
+        // Console.Write("이름: " + playerName);
+        // Console.Write(" 레벨: " + level);
+        // Console.Write(" HP: " + hp);
+        // Console.Write(" 사망 여부: " + isDead);
+        // Console.Write();
         
         // 문자열 연결 (+)
+        Console.WriteLine("이름: " + playerName);
         Console.WriteLine("레벨: " + level);
         Console.WriteLine("HP: " + hp);
+        Console.WriteLine("사망 여부: " + isDead);
+        Console.WriteLine();
+        
+        level = level + 1;
         
         // 문자열 보간 ($"...")
-        Console.WriteLine($"레벨: {level}");
-        Console.WriteLine($"HP: {hp}");
+        // Console.WriteLine($"이름: {playerName}");
+        // Console.WriteLine($"레벨: {level}");
+        // Console.WriteLine($"HP: {hp}");
+        // Console.WriteLine($"사망 여부: {isDead}");
+        Console.WriteLine($"이름 : {playerName}, 레벨 : {level}, HP : {hp}, 사망 여부: {isDead}");
         
     }
 }
